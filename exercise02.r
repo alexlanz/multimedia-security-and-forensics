b17ef3e9fb8963a11aa41ef5f141d287e0f8b409
Alex Lanz <alex@krumer.it>
2015-04-21 13:55:09 +0200

    Exercise 02

--- exercise02.r
+++ exercise02.r
@@ -1,35 +1,38 @@
 require("MMSec")
 
-img1 <- load.png("data/Img7.png")
+img1 <- load.png("data/Img8.png")
+m = 255 * as.matrix.greymap(img1)
 
-m = 256 * as.matrix.greymap(img1)
-h = hist(m, breaks=256, plot=FALSE)
+# Histogram
+png(filename="outputs/Img8.png")
+h = hist(m, breaks=256)
+dev.off()
 
+# Color frequency
+frequency = matrix(0, 256)
+
+for (i in 1:ncol(m)) {
+	for (j in 1:nrow(m)) {
+		color = m[i,j] + 1
+		frequency[color] <- frequency[color] + 1
+	}
+}
+
+# X²
 x = 0
 
 for (i in 0:127) {
-	value1 = 0
-	value2 = 0
+	# Calculate X²
+	y1 = frequency[2*i+1]
+	y2 = frequency[2*i+2]
 
-	for (j in 1:length(h$breaks)) {
-
-		if (h$breaks[j] == (2*i)) {
-			value1 = h$counts[j]
-		}
-
-		if (h$breaks[j] == (2*i+1)) {
-			value2 = h$counts[j]
-		}
-		
-	}
-
-	e = (value1 + value2)/2
+	e = (y1 + y2)/2
 
 	if (e == 0) {
 		next
 	}
 
-	x = x + ((value2 - e)^2 / e)
+	x = x + ((y2 - e)^2 / e)
 }
 
 print(x)
