acdb6fc329701b14b961bcb1774562d672801c9b
Alex Lanz <alex@krumer.it>
2015-04-21 08:54:08 +0200

    Adding error rate line

--- exercise01.r
+++ exercise01.r
@@ -1,9 +1,10 @@
 require("ROCR")
 
 data <- read.table("data/question1.txt", sep=",", header=FALSE)
-pred <- prediction(data[,1], data[,2])
+pred <- prediction(data[,1], data[,2], c("cover", "stego"))
 perf <- performance(pred, measure = "tpr", x.measure = "fpr")
 
 png(filename="outputs/exercise01.png")
-plot(perf, col=rainbow(10))
+plot(perf, col="blue")
+abline(1, -1, col="red")
 dev.off()
